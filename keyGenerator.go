8ed6e5183f13b9f939353c121e718fa83916e20f
multicus <zimmerjan@outlook.com>
2018-11-09 14:53:04 +0100

    Fixed keygen

--- keyGenerator.go
+++ keyGenerator.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
@@ -25,7 +24,7 @@ func generateNewKey() (key string) {
 	var newKey string
 
 	for i := 0; i < 20; i++ {
-		newKey += strconv.Itoa(genRandInt())
+		newKey += chars[genRandInt()]
 	}
 
 	log.Println("Generated a new key: " + newKey)
