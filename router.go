eb6e6ba0e5a1ce8fd1687448665315f565d72cdc
newtpuandre <andgun96@gmail.com>
2018-11-13 12:15:32 +0100

    Moved routes

--- router.go
+++ router.go
@@ -21,4 +21,7 @@ func addRoutes(r *mux.Router) {
 	//Routes located in routes.go
 	r.HandleFunc("/api/{serverid}/rss", addRss).Methods("POST") // Subscribe to a new rss feed on {serverid}
 	r.HandleFunc("/api/{serverid}/rss", listRss).Methods("GET") // List all rss feeds currently subscribed on {serverid}
+	r.HandleFunc("/api/{apiKey}", addRss).Methods("POST")
+	r.HandleFunc("/api/rss", listAllRss).Methods("GET")
+	r.HandleFunc("/api/rss/{apiKey}", listRss).Methods("GET")
 }
